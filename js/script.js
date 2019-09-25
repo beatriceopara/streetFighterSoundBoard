f2559b3dd45b8ae4c725e6b92da751547c4fe1c0
Beatrice <beatriceopara94@gmail.com>
2019-09-25 21:50:14 +0100

    Update script.js
    added js to input image button

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ bisonButton.addEventListener("click", function() {
   bisonAudio.currentTime = 0;
   bisonAudio.play("mBisonAudio");
 });
+
+
+var guileAudio = document.getElementById("guileAudio");
+var guileButton = document.getElementById("guileButton");
+guileButton.addEventListener("click", function() {
+  guileAudio.currentTime = 0;
+  guileAudio.play("guileAudio");
+});
