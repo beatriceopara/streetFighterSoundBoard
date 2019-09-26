41383a7a4d5c2c61ec44fa054f228ba39f98580a
Beatrice <beatriceopara94@gmail.com>
2019-09-26 02:33:07 +0100

    completed
    added all audio files
    linked to js script

--- js/script.js
+++ js/script.js
@@ -1,15 +1,258 @@
-
 var bisonAudio = document.getElementById("mBisonAudio");
 var bisonButton = document.getElementById("mBisonButton");
 bisonButton.addEventListener("click", function() {
   bisonAudio.currentTime = 0;
-  bisonAudio.play("mBisonAudio");
+  bisonAudio.play();
 });
-
 
 var guileAudio = document.getElementById("guileAudio");
 var guileButton = document.getElementById("guileButton");
 guileButton.addEventListener("click", function() {
   guileAudio.currentTime = 0;
-  guileAudio.play("guileAudio");
+  guileAudio.play();
+});
+
+var charlieAudio = document.getElementById("charlieAudio");
+var charlieButton = document.getElementById("charlieButton");
+charlieButton.addEventListener("click", function() {
+  charlieAudio.currentTime = 0;
+  charlieAudio.play();
+});
+
+var chunLiAudio = document.getElementById("chunLiAudio");
+var chunLiButton = document.getElementById("chunLiButton");
+chunLiButton.addEventListener("click", function() {
+  chunLiAudio.currentTime = 0;
+  chunLiAudio.play();
+});
+
+var sagatAudio = document.getElementById("sagatAudio");
+var sagatButton = document.getElementById("sagatButton");
+sagatButton.addEventListener("click", function() {
+  sagatAudio.currentTime = 0;
+  sagatAudio.play();
+});
+
+var feiLongAudio = document.getElementById("feiLongAudio");
+var feiLongButton = document.getElementById("feiLongButton");
+feiLongButton.addEventListener("click", function() {
+  feiLongAudio.currentTime = 0;
+  feiLongAudio.play();
+});
+
+var deeJayAudio = document.getElementById("deeJayAudio");
+var deeJayButton = document.getElementById("deeJayButton");
+deeJayButton.addEventListener("click", function() {
+  deeJayAudio.currentTime = 0;
+  deeJayAudio.play();
+});
+
+var danAudio = document.getElementById("danAudio");
+var danButton = document.getElementById("danButton");
+danButton.addEventListener("click", function() {
+  danAudio.currentTime = 0;
+  danAudio.play();
+});
+
+var guyAudio = document.getElementById("guyAudio");
+var guyButton = document.getElementById("guyButton");
+guyButton.addEventListener("click", function() {
+  guyAudio.currentTime = 0;
+  guyAudio.play();
+});
+
+var rMikaAudio = document.getElementById("rMikaAudio");
+var rMikaButton = document.getElementById("rMikaButton");
+rMikaButton.addEventListener("click", function() {
+  rMikaAudio.currentTime = 0;
+  rMikaAudio.play();
+});
+
+var birdieAudio = document.getElementById("birdieAudio");
+var birdieButton = document.getElementById("birdieButton");
+birdieButton.addEventListener("click", function() {
+  birdieAudio.currentTime = 0;
+  birdieAudio.play();
+});
+
+var akumaAudio = document.getElementById("akumaAudio");
+var akumaAudioButton = document.getElementById("akumaButton");
+akumaButton.addEventListener("click", function() {
+  akumaAudio.currentTime = 0;
+  akumaAudio.play();
+});
+
+var evilRyuAudio = document.getElementById("evilRyuAudio");
+var evilRyuButton = document.getElementById("evilRyuButton");
+evilRyuButton.addEventListener("click", function() {
+  evilRyuAudio.currentTime = 0;
+  evilRyuAudio.play();
+});
+
+var ryuAudio = document.getElementById("ryuAudio");
+var ryuButton = document.getElementById("ryuButton");
+ryuButton.addEventListener("click", function() {
+  ryuAudio.currentTime = 0;
+  ryuAudio.play();
+});
+
+var eHondaAudio = document.getElementById("eHondaAudio");
+var eHondaButton = document.getElementById("eHondaButton");
+eHondaButton.addEventListener("click", function() {
+  eHondaAudio.currentTime = 0;
+  eHondaAudio.play();
+});
+
+var codyAudio = document.getElementById("codyAudio");
+var codyButton = document.getElementById("codyButton");
+codyButton.addEventListener("click", function() {
+  codyAudio.currentTime = 0;
+  codyAudio.play();
+});
+
+var karinAudio = document.getElementById("karinAudio");
+var karinButton = document.getElementById("karinButton");
+karinButton.addEventListener("click", function() {
+  karinAudio.currentTime = 0;
+  karinAudio.play();
+});
+
+var vegaAudio = document.getElementById("vegaAudio");
+var vegaButton = document.getElementById("vegaButton");
+vegaButton.addEventListener("click", function() {
+  vegaAudio.currentTime = 0;
+  vegaAudio.play();
+});
+
+var blankaAudio = document.getElementById("blankaAudio");
+var blankaButton = document.getElementById("blankaButton");
+blankaButton.addEventListener("click", function() {
+  blankaAudio.currentTime = 0;
+  blankaAudio.play();
+});
+
+var kenAudio = document.getElementById("kenAudio");
+var kenButton = document.getElementById("kenButton");
+kenButton.addEventListener("click", function() {
+  kenAudio.currentTime = 0;
+  kenAudio.play();
+});
+
+var juliAudio = document.getElementById("juliAudio");
+var juliButton = document.getElementById("juliButton");
+juliButton.addEventListener("click", function() {
+  juliAudio.currentTime = 0;
+  juliAudio.play();
+});
+
+var dhalsimAudio = document.getElementById("dhalsimAudio");
+var dhalsimButton = document.getElementById("dhalsimButton");
+dhalsimButton.addEventListener("click", function() {
+  dhalsimAudio.currentTime = 0;
+  dhalsimAudio.play();
+});
+
+var sodomAudio = document.getElementById("sodomAudio");
+var sodomButton = document.getElementById("sodomButton");
+sodomButton.addEventListener("click", function() {
+  sodomAudio.currentTime = 0;
+  sodomAudio.play();
+});
+
+var cammyAudio = document.getElementById("cammyAudio");
+var cammyButton = document.getElementById("cammyButton");
+cammyButton.addEventListener("click", function() {
+  cammyAudio.currentTime = 0;
+  cammyAudio.play();
+});
+
+var adonAudio = document.getElementById("adonAudio");
+var adonButton = document.getElementById("adonButton");
+adonButton.addEventListener("click", function() {
+  adonAudio.currentTime = 0;
+  adonAudio.play();
+});
+
+var zangiefAudio = document.getElementById("zangiefAudio");
+var zangiefButton = document.getElementById("zangiefButton");
+zangiefButton.addEventListener("click", function() {
+  zangiefAudio.currentTime = 0;
+  zangiefAudio.play();
+});
+
+var juniAudio = document.getElementById("juniAudio");
+var juniButton = document.getElementById("juniButton");
+juniButton.addEventListener("click", function() {
+  juniAudio.currentTime = 0;
+  juniAudio.play();
+});
+
+var yunAudio = document.getElementById("yunAudio");
+var yunButton = document.getElementById("yunButton");
+yunButton.addEventListener("click", function() {
+  yunAudio.currentTime = 0;
+  yunAudio.play();
+});
+
+var tHawkAudio = document.getElementById("tHawkAudio");
+var tHawkButton = document.getElementById("tHawkButton");
+tHawkButton.addEventListener("click", function() {
+  tHawkAudio.currentTime = 0;
+  tHawkAudio.play();
+});
+
+var rolentoAudio = document.getElementById("rolentoAudio");
+var rolentoButton = document.getElementById("rolentoButton");
+rolentoButton.addEventListener("click", function() {
+  rolentoAudio.currentTime = 0;
+  rolentoAudio.play();
+});
+
+var sakuraAudio = document.getElementById("sakuraAudio");
+var sakuraButton = document.getElementById("sakuraButton");
+sakuraButton.addEventListener("click", function() {
+  sakuraAudio.currentTime = 0;
+  sakuraAudio.play();
+});
+
+var genAudio = document.getElementById("genAudio");
+var genButton = document.getElementById("genButton");
+genButton.addEventListener("click", function() {
+  genAudio.currentTime = 0;
+  genAudio.play();
+});
+
+var balrogAudio = document.getElementById("balrogAudio");
+var balrogButton = document.getElementById("balrogButton");
+balrogButton.addEventListener("click", function() {
+  balrogAudio.currentTime = 0;
+  balrogAudio.play();
+});
+
+var ingridAudio = document.getElementById("ingridAudio");
+var ingridButton = document.getElementById("ingridButton");
+ingridButton.addEventListener("click", function() {
+  ingridAudio.currentTime = 0;
+  ingridAudio.play();
+});
+
+var eagleAudio = document.getElementById("eagleAudio");
+var eagleButton = document.getElementById("eagleButton");
+eagleButton.addEventListener("click", function() {
+  eagleAudio.currentTime = 0;
+  eagleAudio.play();
+});
+
+var roseAudio = document.getElementById("roseAudio");
+var roseButton = document.getElementById("roseButton");
+roseButton.addEventListener("click", function() {
+  roseAudio.currentTime = 0;
+  roseAudio.play();
+});
+
+var makiAudio = document.getElementById("makiAudio");
+var makiButton = document.getElementById("makiButton");
+makiButton.addEventListener("click", function() {
+  makiAudio.currentTime = 0;
+  makiAudio.play();
 });
